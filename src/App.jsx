5b0ae60ef35a4723ecde875349826ac8b8e7007b
책책 <jeong@jeongui-MacBookPro-2.local>
2024-11-23 19:07:24 +0900

    name change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="p-4 bg-white rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold mb-4 text-center">작은 뱀파이어의 모험</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center">작은 스켈레톤의 모험</h1>
         <GameComponent />
       </div>
     </div>
